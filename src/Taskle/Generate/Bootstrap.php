28c992ef87d749ad82ab50376eeff43dacdd0c92
Andrew Shell <andrew@andrewshell.org>
2014-06-21 04:17:31 -0500

    Updating composer

--- src/Taskle/Generate/Bootstrap.php
+++ src/Taskle/Generate/Bootstrap.php
@@ -4,12 +4,14 @@ namespace Taskle\Generate;
 use Cilex\Provider\Console\ConsoleServiceProvider;
 use Silex\Application;
 use Silex\Provider\TwigServiceProvider;
+use Symfony\Component\Console\Command\Command;
+use Taskle\Generator\Pimple\PimpleAwareInterface;
 
 class Bootstrap
 {
     public static function configure(Application $app)
     {
-        $app['console.name'] = 'TaskleGenerate';
+        $app['console.name'] = 'Taskle Generate';
         $app['console.version'] = '2.0.0';
 
         $consoleServiceProvider = new ConsoleServiceProvider;
@@ -20,5 +22,18 @@ class Bootstrap
             // $twig->addExtension(new BootstrapFormExtension());
             return $twig;
         });
+
+        $commands = array(
+            new Command\CreateAction(),
+        );
+
+        foreach ($commands as $command) {
+            if ($command instanceof PimpleAwareInterface) {
+                $command->setContainer($app);
+            }
+            if ($command instanceof Command) {
+                $app['console']->add($command);
+            }
+        }
     }
 }
